19d7eaaa81b35a3c03c4abadaeb5126928770801
周东 <zhoudong@hkgo.vip>
2023-08-18 17:55:17 +0800

    feat: init project

---
+++ components/newComerDiscounts/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="newComerDiscounts-box boxsz">
+		<view class="newComer-title flex alient-item-c justify-item-c">
+			<view class="left">新人现时优惠</view>
+			<view class="midle flex1">9.99元起</view>
+			<text class="more">More ></text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.newComerDiscounts-box{
+		width: 702rpx;
+		height: 840rpx;
+		background: linear-gradient(180deg, #FF9822 0%, #FFBD52 100%);
+		border-radius: 16rpx;
+		margin: 24rpx auto 0 auto;
+		padding: 16rpx;
+		.newComer-title{
+			color: #fff;
+			padding-top: 16rpx;
+			.left{
+				font-size: 40rpx;
+				font-weight: 600;
+				text-shadow: 0px 2px 4px #F86509;
+				padding-right: 16rpx;
+			}
+			.midle{
+				font-size: 32rpx;
+				font-weight: 400;
+				padding-left: 16rpx;
+				border-left: 1rpx solid rgba(255,255,255,.3);
+			}
+			.more{
+				font-size: 28rpx;
+				font-weight: 500;
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,80 @@
+<template>
+	<view class="indexBox">
+		<uni-nav-bar color="#fff" :shadow="false" backgroundColor="#fd93c7" @clickLeft="onClickLeft" @clickTitle="onClickTitle" @clickRight="onClickRight" shadow statusBar shadow="false">
+			<view slot="left" class="index-topheader-left flex flex1 alient-item-c justify-item-c">
+				<image class="index-topheader-logo" src="../../static/index_logo.png"></image>
+			</view>
+			<view class="index-topheader-center flex alient-item-c justify-item-c">
+				<image class="index-topheader-pic" src="../../static/index_search.png"></image>
+				<image class="index-topheader-pic" src="../../static/index_favorite.png"></image>
+				<image class="index-topheader-pic" src="../../static/index_message.png"></image>
+			</view>
+			<view slot="right" class="index-topheader-right flex alient-item-c justify-item-c">
+				<image class="index-topheader-pic" src="../../static/index_customer.png"></image>
+			</view>
+		</uni-nav-bar>
+		
+		<!--新人限時優惠-->
+		<newComerDiscounts />
+	</view>
+</template>
+
+<script>
+	import newComerDiscounts from '../../components/newComerDiscounts/index.vue'
+	export default {
+		components: {
+			newComerDiscounts
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {},
+		methods: {
+			onClickLeft () {
+				uni.showToast({
+					title:'ok-ok',
+					icon:'none'
+				})
+			},
+			onClickTitle () {
+				uni.showToast({
+					title:'ok-ok',
+					icon:'none'
+				})
+			},
+			onClickRight () {
+				uni.showToast({
+					title:'ok-ok',
+					icon:'none'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.indexBox{
+		.index-topheader-center{
+			display: flex;
+			justify-items: end;
+			margin-left:auto;
+			.index-topheader-pic{
+				margin-left:30rpx;
+			}
+		}
+		.index-topheader-right{
+			flex: 1;
+			width: 320rpx;
+		}
+		.index-topheader-logo{
+			width: 90rpx;
+			height: 64rpx;
+		}
+		.index-topheader-pic{
+			width:45rpx;
+			height: 45rpx;
+		}
+	}
+</style>
